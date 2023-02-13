8591b7658ae074e84e402cc4bbdec0097e3ef889
Dreiton Washington <96394627+DreitonWashington@users.noreply.github.com>
2023-02-13 20:01:09 -0300

    Delete ExeVendasMod37 directory

--- ExeVendasMod37/src/main/java/br/com/vendas/dao/jpa/IClienteJpaDAO.java
+++
@@ -1,9 +0,0 @@
-package br.com.vendas.dao.jpa;
-
-import br.com.vendas.dao.generic.jpa.IGenericJapDAO;
-import br.com.vendas.domain.jpa.Persistente;
-
-
-public interface IClienteJpaDAO<T extends Persistente> extends IGenericJapDAO<T, Long>{
-
-}
--- ExeVendasMod37/src/main/java/br/com/vendas/dao/jpa/IProdutoJpaDAO.java
+++
@@ -1,9 +0,0 @@
-package br.com.vendas.dao.jpa;
-
-import br.com.vendas.dao.generic.jpa.IGenericJapDAO;
-import br.com.vendas.domain.jpa.ProdutoJpa;
-
-
-public interface IProdutoJpaDAO extends IGenericJapDAO<ProdutoJpa, Long>{
-
-}
